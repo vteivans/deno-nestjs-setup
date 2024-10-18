9a18897afdc8e77d74b4a2db63634f288df355de
Viesturs Teivāns <vteivans@gmail.com>
2024-10-18 21:27:45 +0300

    Deno feature

--- src/prisma-factory.provider.ts
+++ src/prisma-factory.provider.ts
@@ -1,6 +1,7 @@
 import p from "@prisma/client";
 
 // https://www.prisma.io/docs/orm/prisma-client/deployment/edge/deploy-to-deno-deploy
+// https://github.com/prisma/prisma/releases/tag/4.5.0
 export const prismaProvider = {
     provide: p.PrismaClient,
     useFactory: () => {
